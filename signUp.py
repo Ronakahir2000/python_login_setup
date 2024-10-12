fe82557d56e55fe0ec0076332a32f57305a0049f
Ronakahir2000 <ronakahir98@gmail.com>
2024-10-11 22:04:24 -0400

    Add files via upload

--- signUp.py
+++ signUp.py
@@ -8,11 +8,11 @@ background = ImageTk.PhotoImage(file="bg.jpg")
 bgLabel = Label(signUp_window, image=background)
 bgLabel.grid()
 
-frame = Frame(signUp_window)
+frame = Frame(signUp_window, bg='white')
 frame.place(x=554, y=100)
 
 heading = Label(frame, text='CREATE AN ACCOUNT', font=(
-    'times new roman', 23, 'bold'), bg='white', fg='firebrick1')
-heading.grid(row=0, column=0)
+    'times new roman', 18, 'bold'), bg='white', fg='firebrick1')
+heading.grid(row=0, column=0, padx=10, pady=10)
 
 signUp_window.mainloop()
